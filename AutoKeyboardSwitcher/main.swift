427d9f9c368401db9ccdf4f5b02ff967d85e0d43
Shangxin Guo <int123c@gmail.com>
2018-11-14 23:58:37 +0800

    Commit

--- AutoKeyboardSwitcher/main.swift
+++ AutoKeyboardSwitcher/main.swift
@@ -1,12 +1,39 @@
-//
-//  main.swift
-//  AutoKeyboardSwitcher
-//
-//  Created by Shangxin Guo on 2018/11/14.
-//  Copyright © 2018 Shangxin Guo. All rights reserved.
-//
+import AppKit
+import Carbon
 
-import Foundation
+typealias Pref = [String: String]
 
-print("Hello, World!")
+class Observer: NSObject {
+    lazy var pref: Pref = {
+        let home = FileManager.default.homeDirectoryForCurrentUser.path
+        guard let data = FileManager().contents(atPath: "\(home)/Documents/AutoKeyboardSwitcher.json") else { fatalError() }
+        let decoder = JSONDecoder()
+        let result = try! decoder.decode(Pref.self, from: data)
+        return result
+    }()
+    
+    @objc func handleChange(_ noti: NSNotification) {
+        guard let info = noti.userInfo?[NSWorkspace.applicationUserInfoKey] as? NSRunningApplication else { return }
+        guard let identifier = info.bundleIdentifier else { return }
+        assert({ print(identifier); return true }())
+        
+        if let language = pref[identifier] {
+            guard let source = TISCopyInputSourceForLanguage(language as CFString) else { return }
+            let value = source.takeRetainedValue()
+            TISSelectInputSource(value)
+        }
+    }
+    
+    deinit {
+        NSWorkspace.shared.notificationCenter.removeObserver(self)
+    }
+}
 
+let observer = Observer()
+NSWorkspace.shared.notificationCenter.addObserver(
+    observer,
+    selector: #selector(Observer.handleChange(_:)),
+    name: NSWorkspace.didActivateApplicationNotification,
+    object: nil)
+
+RunLoop.main.run()
