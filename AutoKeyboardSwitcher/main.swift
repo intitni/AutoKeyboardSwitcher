f0dfae82b084fda56366054e102daf92e5d8ed09
Shangxin Guo <int123c@gmail.com>
2018-11-14 23:38:45 +0800

    Initial Commit

---
+++ AutoKeyboardSwitcher/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AutoKeyboardSwitcher
+//
+//  Created by Shangxin Guo on 2018/11/14.
+//  Copyright © 2018 Shangxin Guo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
